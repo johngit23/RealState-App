6f4aff07e5183127fd84301b5a59d950b6213ca5
Yohannes <jhon.klaus2008@gmail.com>
2024-04-11 16:27:55 +0300

    authentication backend

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,69 @@
+import bcrypt from "bcrypt";
+import prisma from "../lib/prisma.js";
+import jwt from "jsonwebtoken";
+
+export const register = async (req, res) => {
+  const { username, email, password } = req.body;
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await prisma.user.create({
+      data: {
+        username,
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    res.status(201).json("User created successfully");
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Failed to create user" });
+  }
+};
+
+export const login = async (req, res) => {
+  const { username, password } = req.body;
+
+  const age = 1000 * 60 * 60 * 24 * 7;
+
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        username,
+      },
+    });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+      expiresIn: age,
+    });
+
+    res
+      .cookie("token", token, {
+        httpOnly: true,
+        //secure: true,
+        sameSite: "none",
+        maxAge: age,
+      })
+      .status(200)
+      .json({ message: "Login successful" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Failed to login" });
+  }
+};
+
+export const logout = async (req, res) => {
+  res.clearCookie("token").json({ message: "Logout successful" });
+};
--- api/server.js
+++ api/server.js
@@ -1,6 +1,18 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 const app = express();
+
+// routes middleware
+dotenv.config();
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(3000, () => {
   console.log("Server is running on http://localhost:3000 Yay");
