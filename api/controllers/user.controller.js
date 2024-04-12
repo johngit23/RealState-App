4a71f1155da43d9cd91eccd61096a602fb7e41b5
Yohannes <jhon.klaus2008@gmail.com>
2024-04-12 23:21:37 +0300

    fetching, updating and deleting -- backend

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,70 @@
+import prisma from "../lib/prisma.js";
+import bcrypt from "bcrypt";
+
+export const getUsers = async (req, res) => {
+  try {
+    const users = await prisma.user.findMany();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: "Failed to get users" });
+  }
+};
+
+export const getUser = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const user = await prisma.user.findUnique({
+      where: { id },
+    });
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: "Failed to get user" });
+  }
+};
+
+export const updateUser = async (req, res) => {
+  const id = req.params.id;
+  const tokenUserId = req.userId;
+  const { password, avatar, ...inputs } = req.body;
+  if (id !== tokenUserId) {
+    return res
+      .status(403)
+      .json({ message: "You are not authorized to update this user" });
+  }
+  let updatedPassword = null;
+  if (password) {
+    updatedPassword = await bcrypt.hash(password, 10);
+  }
+  try {
+    const updatedUser = await prisma.user.update({
+      where: { id },
+      data: {
+        ...inputs,
+        ...(updatedPassword && { password: updatedPassword }),
+        ...(avatar && { avatar }),
+      },
+    });
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    res.status(500).json({ message: "Failed to update user" });
+  }
+};
+
+export const deleteUser = async (req, res) => {
+  const id = req.params.id;
+  const tokenUserId = req.userId;
+
+  if (id !== tokenUserId) {
+    return res
+      .status(403)
+      .json({ message: "You are not authorized to delete this user" });
+  }
+  try {
+    const deletedUser = await prisma.user.delete({
+      where: { id },
+    });
+    res.status(200).json(deletedUser);
+  } catch (error) {
+    res.status(500).json({ message: "Failed to delete user" });
+  }
+};
