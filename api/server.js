494cb364ebc3bc215f2ef0824f6dc6d41469d4c6
Yohannes <jhon.klaus2008@gmail.com>
2024-04-15 15:35:29 +0300

    chat and message controllers and routes -- backend

--- api/server.js
+++ api/server.js
@@ -6,6 +6,8 @@ import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
 import testRoutes from "./routes/test.route.js";
 import postRoutes from "./routes/post.route.js";
+import chatRoutes from "./routes/chat.route.js";
+import messageRoutes from "./routes/message.route.js";
 
 const app = express();
 
@@ -19,6 +21,8 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/test", testRoutes);
+app.use("/api/chat", chatRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(3000, () => {
   console.log("Server is running on http://localhost:3000 Yay");
